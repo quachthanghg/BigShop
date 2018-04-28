a080ca3a266871e83af1d64747b5a71a86ca9bf9
THANGQV\Quach Thang <quachthanghg@gmail.com>
2018-04-28 08:29:27 +0700

    Register User

--- BigShop.Web/Controllers/AccountController.cs
+++ BigShop.Web/Controllers/AccountController.cs
@@ -8,6 +8,9 @@ using System.Web;
 using System.Web.Mvc;
 using static BigShop.Web.App_Start.IdentityConfig;
 using Microsoft.AspNet.Identity.Owin;
+using CaptchaMvc.HtmlHelpers;
+using CaptchaMvc;
+using BigShop.Common;
 
 namespace BigShop.Web.Controllers
 {
@@ -15,11 +18,7 @@ namespace BigShop.Web.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
-
-        public AccountController()
-        {
-        }
-
+        
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
@@ -62,39 +61,50 @@ namespace BigShop.Web.Controllers
         [HttpPost]
         public async Task<ActionResult> Register(RegisterViewModel registerViewModel)
         {
-            if (ModelState.IsValid)
+            if (this.IsCaptchaValid("Sai mã captch !!! Vui lòng nhập lại"))
             {
-                var email = await _userManager.FindByEmailAsync(registerViewModel.Email);
-                if (email != null)
+                if (ModelState.IsValid)
                 {
-                    ModelState.AddModelError("email","Email đã tồn tại");
-                    return View(registerViewModel);
-                }
+                    var email = await _userManager.FindByEmailAsync(registerViewModel.Email);
+                    if (email != null)
+                    {
+                        ModelState.AddModelError("email", "Email đã tồn tại");
+                        return View(registerViewModel);
+                    }
 
-                var userName = await _userManager.FindByNameAsync(registerViewModel.UserName);
-                if (userName != null)
-                {
-                    ModelState.AddModelError("userName", "Tài khoản đã tồn tại");
-                    return View(registerViewModel);
-                }
-                
-                var users = new ApplicationUser()
-                {
-                    UserName = registerViewModel.UserName,
-                    FullName = registerViewModel.FullName,
-                    PasswordHash = registerViewModel.Password,
-                    Email = registerViewModel.Email,
-                    Address = registerViewModel.Address,
-                    PhoneNumber = registerViewModel.PhoneNumber
-                };
-                await _userManager.CreateAsync(users, registerViewModel.Password);
+                    var userName = await _userManager.FindByNameAsync(registerViewModel.UserName);
+                    if (userName != null)
+                    {
+                        ModelState.AddModelError("userName", "Tài khoản đã tồn tại");
+                        return View(registerViewModel);
+                    }
 
-                var adminUser = await _userManager.FindByEmailAsync(registerViewModel.Email);
-                if (adminUser != null)
-                {
-                    await _userManager.AddToRolesAsync(adminUser.Id, new string[] { "User" });
+                    var users = new ApplicationUser()
+                    {
+                        UserName = registerViewModel.UserName,
+                        FullName = registerViewModel.FullName,
+                        PasswordHash = registerViewModel.Password,
+                        Email = registerViewModel.Email,
+                        Address = registerViewModel.Address,
+                        PhoneNumber = registerViewModel.PhoneNumber
+                    };
+                    await _userManager.CreateAsync(users, registerViewModel.Password);
+
+                    var adminUser = await _userManager.FindByEmailAsync(registerViewModel.Email);
+                    if (adminUser != null)
+                    {
+                        await _userManager.AddToRolesAsync(adminUser.Id, new string[] { "User" });
+                    }
+                    TempData["Success"] = "Đăng ký thành công";
+
+                    string content = System.IO.File.ReadAllText(Server.MapPath("~/Assets/Client/template/NewUserTemplate.html"));
+                    content = content.Replace("{{UserName}}", adminUser.FullName);
+                    content = content.Replace("{{Link}}", ConfigHelper.GetByKey("CurrentLink"));
+                    
+                    MailHelper.SendMail(adminUser.Email, "Đăng ký tài khoản thành công", content);
+
                 }
-                 
+                return View();
             }
             return View();
         }
