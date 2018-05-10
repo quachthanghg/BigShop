e7f92333f03b157b31c3b7a3a13cc47b912754cd
THANGQV\Quach Thang <quachthanghg@gmail.com>
2018-05-10 18:33:07 +0700

    fix template and create code statistic

--- BigShop.Web/App/component/statistics/statisticController.js
+++ BigShop.Web/App/component/statistics/statisticController.js
@@ -2,11 +2,7 @@
     app.controller('statisticController', statisticController)
     statisticController.$inject = ['$scope', 'apiService', '$timeout', '$filter']
     function statisticController($scope, apiService, $timeout, $filter) {
-
-        // Simulate async data update
-        $scope.data = [];
-        $scope.labels = [];
-        $scope.series = ['Series A', 'Series B'];
+        
         function getStatistic() {
             var config = {
                 params: {
@@ -15,19 +11,60 @@
                 }
             };
             apiService.get('/Api/Statistic/GetRevenueStatistic', config, function (result) {
-                //var labels = [];
-                //var data = [];
-                //var revenues = [];
-                //var benefits = [];
-                //$.each(result.data, function (i, item) {
-                //    labels.push($filter('date')(item.Date, 'dd/MM/yyyy'));
-                //    revenues.push(item.Revenue);
-                //    benefits.push(item.Benefit);
-                //});
-                //data.push(revenues);
-                //data.push(benefits);
+                var labels = [];
+                var revenues = [];
+                var benefits = [];
+              
+                $.each(result.data, function (i, item) {
+                    labels.push($filter('date')(item.Date, 'dd/MM/yyyy'));
+                    revenues.push(item.Revenue);
+                    benefits.push(item.Benefit);
+                });
 
-                $scope.data = result.data;
+                Highcharts.chart('container', {
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: 'Monthly Average Rainfall'
+                    },
+                    subtitle: {
+                        text: 'Source: WorldClimate.com'
+                    },
+                    xAxis: {
+                        categories: labels,
+                        crosshair: true
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: 'Doanh thu ($$$)'
+                        }
+                    },
+                    tooltip: {
+                        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                        footerFormat: '</table>',
+                        shared: true,
+                        useHTML: true
+                    },
+                    plotOptions: {
+                        column: {
+                            pointPadding: 0.2,
+                            borderWidth: 0
+                        }
+                    },
+                    series: [{
+                        name: 'Doanh thu',
+                        data: revenues
+
+                    }, {
+                        name: 'Lợi nhuận',
+                        data: benefits
+
+                    }]
+                });
                 //$scope.labels = labels;
             }, function () {
                 console.log("Error !");
